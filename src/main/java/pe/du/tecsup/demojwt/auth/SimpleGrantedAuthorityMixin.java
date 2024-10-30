59322839997424b10121bb940595771ba8cdbe6c
Contreras Perales Rodrigo <levicom134@gmail.com>
2024-10-30 14:18:02 -0500

    Granted Auth Mixin

--- src/main/java/pe/du/tecsup/demojwt/auth/SimpleGrantedAuthorityMixin.java
+++ src/main/java/pe/du/tecsup/demojwt/auth/SimpleGrantedAuthorityMixin.java
@@ -1,4 +1,11 @@
 package pe.du.tecsup.demojwt.auth;
 
-public class SimpleGrantedAuthorityMixin {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public abstract class SimpleGrantedAuthorityMixin {
+
+    @JsonCreator
+    public SimpleGrantedAuthorityMixin(@JsonProperty("authority") String role) {}
+
 }
